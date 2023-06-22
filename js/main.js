56ae76a600d3dbdb45f07df53c1e418de51150b9
bccarruth927 <brecarr1997@aol.com>
2023-06-22 12:07:08 -0400

    Add rest of calculators and their functionality

--- js/main.js
+++ js/main.js
@@ -2,14 +2,53 @@
 $(document).ready(() => {
     //Hide all calculator containers when the web app loads
     $('#flatwork-calculator').hide();
+    $('#round-work-calculator').hide();
+    $('#wall-footer-calculator').hide();
+    $('#stairs-calculator').hide();
+    $('#curb-gutter-calculator').hide();
+    $('#drain-tunnel-calculator').hide();
+
+
 
     //Event handler attached to the carets of the calculator containers
     $('#flatwork-calculator-caret').on('click', () => {
         $('#flatwork-calculator').slideToggle();
     });
+    $('#round-work-calculator-caret').on('click', () => {
+        $('#round-work-calculator').slideToggle();
+    });
+    $('#wall-footer-calculator-caret').on('click', () => {
+        $('#wall-footer-calculator').slideToggle();
+    });
+    $('#stairs-calculator-caret').on('click', () => {
+        $('#stairs-calculator').slideToggle();
+    });
+    $('#curb-gutter-calculator-caret').on('click', () => {
+        $('#curb-gutter-calculator').slideToggle();
+    });
+    $('#drain-tunnel-calculator-caret').on('click', () => {
+        $('#drain-tunnel-calculator').slideToggle();
+    });
+
+
 
     //Event handler attached to the calculator container that hides the container when mouse leaves it (desktop only)
-    $('#flatwork-container').on('mouseleave', () => {
+    $('#flatwork-calculator').on('mouseleave', () => {
         $('#flatwork-calculator').slideUp();
+    });
+    $('#round-work-calculator').on('mouseleave', () => {
+        $('#round-work-calculator').slideUp();
+    });
+    $('wall-footer-calculator').on('mouseleave', () => {
+        $('#wall-footer-calculator').slideUp();
+    });
+    $('#stairs-calculator').on('mouseleave', () => {
+        $('#stairs-calculator').slideUp();
+    });
+    $('#curb-gutter-calculator').on('mouseleave', () => {
+        $('#curb-gutter-calculator').slideUp();
+    });
+    $('#drain-tunnel-calculator').on('mouseleave', () => {
+        $('#drain-tunnel-calculator').slideUp();
     });
 });
